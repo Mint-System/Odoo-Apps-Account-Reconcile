075aba7e5a31209fb745b741bd7a46c882bddb71
Janik von Rotz <login@janikvonrotz.ch>
2025-01-28 18:05:37 +0100

    chore: update author

--- account_accountant_reconcile/__manifest__.py
+++ account_accountant_reconcile/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
         Reconcile invoices with matching amount and reference.
     """,
-    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "author": "Mint System GmbH",
     "website": "https://www.mint-system.ch",
     "category": "Invoicing",
     "version": "16.0.1.0.0",
